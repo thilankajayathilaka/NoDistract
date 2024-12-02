4c57a854afa2e9f839f8018bb2ec32d2017d5473
thilankas961 <thilankas961@gmail.com>
2024-12-02 19:49:21 +0530

    hide the filters

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'hideContent') {
-        // Hide or show the content based on the checkbox status
-        const isChecked = request.isChecked;
-        const contents = document.querySelector('#contents');
-        
-        if (isChecked) {
-            contents.style.display = 'none';  // Hide content
-        } else {
-            contents.style.display = 'block'; // Show content
-        }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "hideContent") {
+    // Hide or show the content based on the checkbox status
+    const isChecked = request.isChecked;
+    const contents = document.querySelectorAll("#contents, #chips-content");
+
+    if (isChecked) {
+      contents.forEach((content) => {
+        content.style.display = "none"; // Hide content
+      });
+    } else {
+      contents.forEach((content) => {
+        content.style.display = "block"; // Show content
+      });
     }
+  }
 });
